92f21572a2e1d0815a487c18363e2f0d20ae6852
lananh2801 <lanbeu2801@gmail.com>
2022-11-04 14:40:55 +0700

    function update User

---
+++ src/main/java/com/example/clothes/Exception/CustomExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.clothes.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(NotFoundException ex, WebRequest re) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+}
---
+++ src/main/java/com/example/clothes/Exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.clothes.Exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+    private String message;
+}
